8f9a94a356de3b15c31d0b93e573a401735934cd
Joe VanLeeuwen <joevanleeuwen@Joes-MacBook-Air.local>
2013-08-31 00:24:56 -0400

    finished my koans

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,25 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
+      function findMushrooms(x) {return x === "mushrooms"}
+
+      var productsICanEat = _(products).filter(function(o){ 
+        if(o.containsNuts === false) {
+          if(!(_(o.ingredients).any(findMushrooms))) {
+            return o;
+          };
+        };
+      });
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +64,25 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+      var sum = _([_.range(3, 1000, 3), _.range(5, 1000, 5)]).chain()
+        .flatten()
+        .reduce(function(memo, num){
+          if(num % 3 === 0 & num % 5 === 0) {
+            return memo + (num*.5); 
+          } else {
+            return memo + num;
+          }; 
+        })
+        .value();
 
-    expect(233168).toBe(FILL_ME_IN);
+       /* try chaining range() and reduce() */
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,39 +95,58 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
+    // Possibility #one
+    // _.map(products, function(i){ 
+    //   return _.map(i.ingredients, function(j) {
+    //     return ingredientCount[j] = (ingredientCount[j] || 0) + 1;
+    //   }) 
+    // });
+
+    // Possibility #two
+    _(products).chain()
+      .map(function(i) {
+        return i.ingredients;
+      })
+      .flatten()
+      .map(function(j) {
+        ingredientCount[j] = (ingredientCount[j] || 0) + 1;
+      });
+
+    // Can't figure out how to throw reduce in and make things work
+
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
-  /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
+  // /* UNCOMMENT FOR EXTRA CREDIT */
+  
+  // it("should find the largest prime factor of a composite number", function () {
 
-  });
+  // });
 
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+  // it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
 
-  });
+  // });
 
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+  // it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
 
 
-  });
+  // });
 
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
+  // it("should find the difference between the sum of the squares and the square of the sums", function () {
 
-  });
+  // });
 
-  it("should find the 10001st prime", function () {
+  // it("should find the 10001st prime", function () {
 
-  });
-  */
+  // });
+  
 });
